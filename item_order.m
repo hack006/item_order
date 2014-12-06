314dea5491bd52c59bfe53fe0a8cbe9c7eef6570
Ondrej Janata <o.janata@gmail.com>
2014-12-07 00:38:01 +0100

    Added manual data setup dialog - working - todo semirandom data generation not implemented - todo code refactoring and small improvements

--- item_order.m
+++ item_order.m
@@ -93,12 +93,12 @@ classdef item_order
             % %%%%
             % WORKAROUND - direct passing arguments failed
             % %%%%
-            string_to_memorize = varargin{1,1}
-            string_to_match = varargin{1,2}
-            tmp = varargin{1,3}
-            change_type = tmp{1}
-            tmp = varargin{1,4}
-            change_position = tmp{1}
+            string_to_memorize = varargin{1,1};
+            string_to_match = varargin{1,2};
+            tmp = varargin{1,3};
+            change_type = tmp{1};
+            tmp = varargin{1,4};
+            change_position = tmp{1};
             % store current trial type - used in result function
             T.current_trial_type = change_type;
             
@@ -124,7 +124,7 @@ classdef item_order
             % %%%%
             % Wait some time to memorize
             % %%%%
-            set(instructions_text, 'string', 'Remember string')
+            set(instructions_text, 'string', 'Remember string');
             set(test_string_label, 'string', string_to_memorize);
             pause(T.memorize_time);
             
@@ -139,7 +139,7 @@ classdef item_order
             % SHOW TEXT FOR COMPARISON
             % %%%%
             tic; % start time measurement
-            set(instructions_text, 'string', 'Same strings - "L"         Different strings - "A"')
+            set(instructions_text, 'string', 'Same strings - "L"         Different strings - "A"');
             set(test_string_label, 'string', string_to_match);
         end
         
@@ -174,6 +174,7 @@ classdef item_order
             T.Results.time(j) = time
             T.Results.correctness(j) = answer %numerical value from 0−1 which describes the quality of the answer
             %in results you can save any further information important to be saved for the task
+            T.Results.type(j) = T.current_trial_type
             T.Results.answer_key(j) = response_key
         end
         
@@ -192,8 +193,8 @@ classdef item_order
             % Generate array of types of trials
             % %%%%
             
-            CHARACTER_POSITION_CHANGE_ARR = linspace(-1,-1,GES.character_change_count)
-            CHARACTER_POSITION_SWAP_ARR = linspace(-1,-1,GES.character_swap_count)
+            CHARACTER_POSITION_CHANGE_ARR = linspace(-1,-1,GES.character_change_count);
+            CHARACTER_POSITION_SWAP_ARR = linspace(-1,-1,GES.character_swap_count);
             
             % generate trial type order
             trials_type_order_source = [linspace(0,0,GES.character_not_touched_count) ...
@@ -223,7 +224,7 @@ classdef item_order
             %{'string_to_memorize', 'string_to_match','change_type','change_position'};
             for i=1:5
                 % append position to CHARACTER_POSITION_CHANGE_ARR
-                tmp = find(CHARACTER_POSITION_CHANGE_ARR < 0)
+                tmp = find(CHARACTER_POSITION_CHANGE_ARR < 0);
                 
                 if isempty(tmp)
                     break
@@ -233,7 +234,7 @@ classdef item_order
                 char_position_change_count = char_position_change_count + division_remainder; % add remainder from previous
                 
                 
-                CHARACTER_POSITION_CHANGE_ARR(tmp(1):floor(tmp(1)+char_position_change_count-1)) = linspace(i,i,floor(char_position_change_count))
+                CHARACTER_POSITION_CHANGE_ARR(tmp(1):floor(tmp(1)+char_position_change_count-1)) = linspace(i,i,floor(char_position_change_count));
                 
                 % update remainder
                 division_remainder = mod(char_position_change_count, 1);
@@ -242,7 +243,7 @@ classdef item_order
             % last trial count is calculated to match change count trials
             
             for i=(sum(CHARACTER_POSITION_CHANGE_ARR~=-1)+1):GES.character_change_count
-                CHARACTER_POSITION_CHANGE_ARR(i) = 6
+                CHARACTER_POSITION_CHANGE_ARR(i) = 6;
             end
             
             
@@ -260,23 +261,23 @@ classdef item_order
             
             for i=1:5
                 % append position to CHARACTER_POSITION_CHANGE_ARR
-                tmp = find(CHARACTER_POSITION_SWAP_ARR < 0)
+                tmp = find(CHARACTER_POSITION_SWAP_ARR < 0);
                 
                 if isempty(tmp)
-                    break
+                    break;
                 end                
                 
                 char_position_swap_count = (T.character_position_change_percentage(i) / percentage_for_one_trial);
                 char_position_swap_count = char_position_swap_count + division_remainder; % add remainder from previous
                 
-                CHARACTER_POSITION_SWAP_ARR(tmp(1):floor(tmp(1)+char_position_swap_count-1)) = linspace(i,i,floor(char_position_swap_count))
+                CHARACTER_POSITION_SWAP_ARR(tmp(1):floor(tmp(1)+char_position_swap_count-1)) = linspace(i,i,floor(char_position_swap_count));
                 
                 % update remainder
                 division_remainder = mod(char_position_swap_count, 1);
             end
             % last trial count is calculated to match swap count trials
             for i=(sum(CHARACTER_POSITION_SWAP_ARR~=-1)+1):GES.character_swap_count
-                CHARACTER_POSITION_SWAP_ARR(i) = 6
+                CHARACTER_POSITION_SWAP_ARR(i) = 6;
             end
             
             % %%%%
@@ -306,8 +307,8 @@ classdef item_order
                         % SWAP CHARS on the (i)-th and (i+1)-th postions
                         swapped_string = shown_string;
                         change_position = CHARACTER_POSITION_SWAP_ARR(swap_counter);
-                        swapped_string(change_position) = shown_string(change_position + 1) 
-                        swapped_string(change_position + 1) = shown_string(change_position) 
+                        swapped_string(change_position) = shown_string(change_position + 1); 
+                        swapped_string(change_position + 1) = shown_string(change_position); 
                         
                         datTask(i,1:4) = {shown_string, swapped_string, 1, change_position};
                         swap_counter = swap_counter + 1;
@@ -316,7 +317,7 @@ classdef item_order
                         swapped_string = shown_string;
                         change_position = CHARACTER_POSITION_SWAP_ARR(change_counter);
                         not_used_chars = setdiff(T.character_set, shown_string);
-                        replacement_char = not_used_chars(randi(size(not_used_chars,2),1,1))
+                        replacement_char = not_used_chars(randi(size(not_used_chars,2),1,1));
                         swapped_string(change_position) = replacement_char;
                         
                         datTask(i,1:4) = {shown_string, swapped_string, 2, change_position};
@@ -328,11 +329,209 @@ classdef item_order
             end
         end
         
-        function datTask=Manual(T,nmbRep)%obligatory
-            % TODO
+         function datTask = Manual(T,nmbRep)         %obligatory
+            datTask=T.Manually_edit(nmbRep);
+         end
+            
+         function datTask = Manually_edit(T,nmbRep)  %obligatory
+            % %%%%
+            % Create dialog
+            % %%%%
+            manualDialog = figure('Name','Manual task setup generator','Units','pixels','Position',[10 10 1000 700],'MenuBar', 'none','Color','white');
+            
+            uicontrol(manualDialog,'Units','normalized',...
+                'Style','text',...
+                'Position',[0.05 0.8 0.9 0.1],...
+                'BackgroundColor','black',...
+                'String','');
+            uicontrol(manualDialog,'Units','normalized',...
+                'Style','text',...
+                'Position',[0.05 0.72 0.9 0.08],...
+                'ForegroundColor','white',...
+                'BackgroundColor','black',...
+                'String','Manual task setup generator. Configure values and press SAVE.',...
+                'FontSize',18);
+            
+            % %%%%
+            %  Basic setup of dialog
+            % %%%%
+            data_types=cell(3, 2);
+            
+            data_types{1} = {'same',0};
+            data_types{2} = {'position swap',1};
+            data_types{3} = {'position change',2};
+           
+            dataCellArray = cell(nmbRep,4);
+            
+            for i=1:nmbRep
+                dataCellArray(i,1) = {''};
+                dataCellArray(i,2) = {''};
+                dataCellArray(i,3) = {'same'};
+                dataCellArray(i,4) = {0};
+                dataCellArray(i,5) = {''};
+                dataCellArray(i,6) = {false};
+                dataCellArray(i,7) = {'fill data'};
+            end;
+                   
+            % Draw table
+            manualConfigTable = uitable(manualDialog,'Data',dataCellArray,...
+                'units','normalized',...
+                'position',[0.05 0.1 0.9 0.6],...
+                'ColumnName', {'Memorize string','Decision string', 'Type of trial', 'Position change', 'Change character', 'Trial syntax ok?', 'Error message'},...
+                'ColumnFormat',{'char','char',{'same' 'position swap' 'position change'},'numeric','char','logical', 'char'},...
+                'ColumnEditable',[true false true true true false false],...
+                'CellEditCallback', @T.manualTableChangedValidationCallback);
+            
+            % Draw control buttons
+            save_btn = uicontrol(manualDialog,'units','normalized','style','pushbutton','string','SAVE', 'FontSize', 8, 'HorizontalAlignment','left','position',[0.8 0.02 0.15 0.05],'callback',@T.manualDataSAVECallback);
+        
+            waitfor(save_btn);
+            
+            % Pass entered datasets
+            dataCellArray = get(manualConfigTable,'Data');
+            for i=1:nmbRep
+                datTask(i,1) = dataCellArray(i,1); % shown string
+                datTask(i,2) = dataCellArray(i,2); % compare to string
+                trial_type_int_val = 0;
+                switch dataCellArray{i,3}
+                    case 'position swap'
+                        trial_type_int_val = 1;
+                    case 'position change'
+                        trial_type_int_val = 2;
+                    otherwise
+                        trial_type_int_val = 0;
+                end
+                datTask(i,3) = {trial_type_int_val}; % change type
+                datTask(i,4) = dataCellArray(i,4); % position change
+            end
+            delete(manualDialog);
+         end
+        
+         function manualTableChangedValidationCallback(T,hObject,eventData)
+           
+            currentTableData = get(hObject, 'Data');
+            selectedRowNum = eventData.Indices(1);
+            selectedRow = currentTableData(selectedRowNum,:);
+            
+            
+            % %%%%
+            % LOAD PARAMS
+            % %%%%
+            memorize_string = selectedRow{1}
+            decision_string = selectedRow{2}
+            trial_type_txt = selectedRow{3}
+            switch trial_type_txt
+                case 'position swap'
+                    trial_type=1;
+                case 'position change'
+                    trial_type=2;
+                otherwise
+                    trial_type=0;
+            end;        
+            position_change = selectedRow{4}
+            new_char = selectedRow{5}
+            
+            % %%%%
+            % TEST if input valid
+            % %%%%
+            trial_setup_correct = false;
+            err_msg = '';
+            if trial_type == 0
+                % lengt must be greater or equal 7
+                if size(memorize_string,2) >= 7
+                    trial_setup_correct = true;
+                    err_msg = '';
+                else
+                    err_msg = 'string must be 7 chars long';
+                end
+            elseif trial_type == 1 || trial_type == 2
+                % lengt must be greater or equal 7
+                if size(memorize_string,2) < 7
+                    trial_setup_correct = false;
+                    err_msg = 'string must be 7 chars long'
+                % position change must be in range 1:6
+                elseif position_change < 1 || position_change > 6 
+                    err_msg = 'change position must be in range {1,..,6}';
+                elseif trial_type == 2 && size(new_char,2) < 1
+                    err_msg = 'enter character for change'
+                else
+                    trial_setup_correct = true;
+                    err_msg = '';
+                end
+            else
+                trial_setup_correct = false;
+            end;
+            
+
+            % @todo
+            
+            % %%%%%
+            % FORMAT OUTPUT
+            % %%%%%
+
+            % cut if text longer then 7 chars 
+            if size(memorize_string,2) > 7
+                memorize_string = memorize_string(1:7);
+            end
+            memorize_string = upper(memorize_string);
+            currentTableData(selectedRowNum,1) = {memorize_string};
+            set(hObject,'Data',currentTableData);
+            
+            if trial_setup_correct == true
+                switch trial_type
+                    case 0
+                        decision_string = memorize_string;
+                    case 1
+                        %swap positions
+                        if position_change > 0 && position_change < 7
+                            decision_string = [...
+                                memorize_string(1:(position_change-1))...
+                                memorize_string(position_change + 1)...
+                                memorize_string(position_change)...
+                                memorize_string((position_change + 2):end)
+                            ];
+                        else
+                            memorize_string = '';
+                        end
+                    case 2
+                        new_char = new_char(1);
+                        %change position
+                        if position_change > 0 && position_change < 7
+                            decision_string = [...
+                                memorize_string(1:(position_change-1))...
+                                new_char...
+                                memorize_string((position_change + 1):end)
+                            ];
+                        else
+                            memorize_string = '';
+                        end
+                    otherwise
+                        trial_type_exception = MException('table:invalid_trial_type',...
+                            'Trial type can be only in range 0..2!!');
+                        raise(trial_type_exception);
+                end
+            end;
+            
+            % update table
+            currentTableData(selectedRowNum,2) = {decision_string};
+            currentTableData(selectedRowNum,6) = {trial_setup_correct};
+            currentTableData(selectedRowNum,7) = {err_msg};
+            set(hObject,'Data',currentTableData);
+               
+            hObject
+            eventData
+         end
+        
+        function manualDataSAVECallback(T,hObject,eventData)
+            delete(hObject);
         end
         
+        
         function datTask=SemiRandom(T,nmbRep)%obligatory
+            datTask=T.Partly_random(nmbRep);
+        end
+        
+        function datTask = Partly_random(T,nmbRep)  %obligatory
             % TODO
         end
         
@@ -364,16 +563,8 @@ classdef item_order
         % %%%%%%%%%%%
         
         function tasks_m=menu(T,tasks_m)%obligatory
-            %can be empty
-            %if any additional submenu with function is required ...
-            %than it can be defined here as follows:
-
-            Task name m=uimenu(tasks_m,'Label','Task_name')
-            %main folder for task submenus with additional information
-            uimenu(task_name_m,'Label','submenu_name','callback',@callback_name)
-            %submenu definition
-            %which will redirect you to function callback name ...
-            %(which must be defined in this file below)
+           % define simple menu holder
+            m=uimenu(tasks_m,'Label','Item order');
         end
     end
     
